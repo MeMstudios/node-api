a1d40730864905181fcd3f1056e8a0fbdf791b1c
MeMstudios <michaelmurphystudios@gmail.com>
2019-06-28 16:12:26 -0400

    switched toMatch function. removed custom function.

--- test/app.test.js
+++ test/app.test.js
@@ -2,29 +2,6 @@ const request = require('supertest');
 const app = require('../src/app');
 const util = require('../src/util');
 
-expect.extend({
-    toMatchISOregex(timestamp) {
-        //Regex should match the ISO 8601 for datetime without milliseconds.  I think the regex can be improved.  We're assuming it's a valid timestamp.
-        const isoRegex = /\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z)/;
-        const pass = isoRegex.test(timestamp);
-
-        if (pass) {
-            return {
-              message: () =>
-                `${timestamp} is in ISO 8601 format!`,
-              pass: true,
-            };
-          } 
-          else {
-            return {
-              message: () =>
-                `expected ${timestamp} to be in ISO 8601 format.  If it is, check the regex.`,
-              pass: false,
-            };
-        }
-    }
-});
-
 describe('Testing the timestamp endpoint', () => {
     test('It should respond to the GET method with 200 status', async () => {
         const response = await request(app).get('/timestamp');
@@ -35,6 +12,8 @@ describe('Testing the timestamp endpoint', () => {
 describe('Test the date function', () => {
     test('It should return the date in UTC in ISO 8601 format', () => {
         const date = util.isoTimestamp();
-        expect(date.timestamp).toMatchISOregex();
+        //Regex should match the ISO 8601 for datetime without milliseconds.  I think the regex can be improved.  We're assuming it's a valid timestamp.
+        const isoRegex = /\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z)/;
+        expect(date.timestamp).toMatch(isoRegex);
     });
 });
